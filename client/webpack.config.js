73a3621169b5d1ba4d9ee0992315d76335f73e42
Meghan Simmons <simmons.meghan@gmail.com>
2023-06-06 00:34:49 -0400

    added Webpack PWA Manifest plugin and logic for the install button

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -28,8 +28,23 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-
       
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('assets/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
